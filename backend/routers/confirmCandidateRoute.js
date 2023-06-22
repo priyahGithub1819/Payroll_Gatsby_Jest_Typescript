c8d8bee3c6e63a561eef7c20e256b5dd2ca17d3b
Gayatri Tajne <gayatritajne15@gmail.com>
2023-06-22 19:06:36 +0530

    cleaned routers folder

--- backend/routers/confirmCandidateRoute.js
+++ backend/routers/confirmCandidateRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { createconfirmCandidate, getconfirmCandidate } = require("../controller/confirmCandidateContol");
+const { createconfirmCandidate } = require("../controller/confirmCandidateContol");
 const {
   isAdmin,
   isAuthenticated,
