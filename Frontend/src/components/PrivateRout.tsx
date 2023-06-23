276a3841a831ecfe8edf12a42f2cef220d768080
praptiGomekar <praptig@uvxcel.com>
2023-06-23 11:32:46 +0530

    Cleaned component files and apiFunction by prapti

--- Frontend/src/components/PrivateRout.tsx
+++ Frontend/src/components/PrivateRout.tsx
@@ -1,38 +1,40 @@
-import React, { useContext, useState, useEffect } from "react"
-import { navigate } from "gatsby"
-import { UserData } from "../components/Layout"
+import React, { useContext, useState, useEffect } from "react";
+import { navigate } from "gatsby";
+import { UserData } from "../components/Layout";
 
+const PrivateRoute = ({
+  isValidRole,
+  component: Component,
+  ...rest
+}: any): any => {
+  const [loadUser, setLoadUser] = useState<any>();
+  const { protectedUser: data, callUser, protectedApi } = useContext(UserData);
 
-const PrivateRoute = ({ isValidRole, component: Component, ...rest }: any):any => {
-    const [loadUser, setLoadUser] = useState<any>()
-    const { protectedUser: data, callUser, protectedApi } = useContext(UserData)
+  useEffect(() => {
+    callUser();
+    protectedApi();
+  }, []);
 
-    useEffect(() => {
-        callUser()
-        protectedApi()
-    }, [])
-
-    useEffect(() => {
-        if (data) {
-            setLoadUser(data)
-            console.log(data)
-        }
-    }, [data])
-
-    if (loadUser) {
-        if (loadUser.success === true) {
-            if (!isValidRole.includes(loadUser.employee.payrollData.role)) {
-                navigate("/app/login")
-                return window.alert(
-                    `${loadUser.employee.payrollData.role} can not be excess this resource`
-                )
-            } else {
-                return <Component {...rest} />
-            }
-        } else {
-            navigate("/app/login")
-            return window.alert(data.error)
-        }
+  useEffect(() => {
+    if (data) {
+      setLoadUser(data);
     }
-}
-export default PrivateRoute
+  }, [data]);
+
+  if (loadUser) {
+    if (loadUser.success === true) {
+      if (!isValidRole.includes(loadUser.employee.payrollData.role)) {
+        navigate("/app/login");
+        return window.alert(
+          `${loadUser.employee.payrollData.role} can not be excess this resource`
+        );
+      } else {
+        return <Component {...rest} />;
+      }
+    } else {
+      navigate("/app/login");
+      return window.alert(data.error);
+    }
+  }
+};
+export default PrivateRoute;
